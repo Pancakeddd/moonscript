4cd882f4f391b464ff3a25378a1c0233c4caab86
Pancakeddd <chaneofcake@gmail.com>
2019-08-19 08:41:11 -0700

    small change with test

--- test.moonp.lua
+++ test.moonp.lua
@@ -1,1 +1,14 @@
-return macro(test)
+local lpeg = require("lpeg")
+local mark
+mark = function(name, ...)
+  return {
+    name,
+    ...
+  }
+end
+local grammar = lpeg.P({
+  "main",
+  main = lpeg.V("factor"),
+  factor = z
+})
+return 
